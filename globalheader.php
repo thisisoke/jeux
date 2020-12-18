8314782de0bf3a25eb876f28c6b82a1231446c57
thisioke <seyitanoke@gmail.com>
2020-12-18 02:24:51 -0500

    Added Font Awesome to access a play button GylphS

--- globalheader.php
+++ globalheader.php
@@ -3,6 +3,10 @@
 <meta content="jeux! is social gaming web app Play fun games with your friends from anywhere" name="description">
 
 <link href="css/main.css" rel="stylesheet" type="text/css">
+<link href="css/all.min.css" rel="stylesheet" type="text/css">
+
+<!-- importing fonts -->
+<link rel="stylesheet" href="https://use.typekit.net/ibk8xny.css">
 
 <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
 <link href="images/webclip.png" rel="apple-touch-icon">
