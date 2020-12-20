4cbffd5cc78c85d98d0aeff943dfd12a7181356e
thisioke <seyitanoke@gmail.com>
2020-12-20 02:47:16 -0500

    completed and tested gamePlayerJoin Process PHP script. can now insert a new player and check if room is ful. outputs 3 2 erro message and 3 success ones

--- gamePlayerJoinProcess.php
+++ gamePlayerJoinProcess.php
@@ -13,33 +13,63 @@ $avatar = $_POST["avatar"];
 include('includes/dbconfig.php');
 
 
-//Find GameRoom
-$stmt = $pdo->prepare("SELECT * FROM `gameRoom` WHERE `gameCode` = '$roomCode'");
-
+//Find The GameRoom
+$stmt = $pdo->prepare("SELECT * FROM `gameRoom` WHERE `roomCode` = '$roomCode'");
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
-//Store Needed Variables
+//Store The gameRoom Id from the game room that was found 
 $gameRoomId = $row["gameRoomId"];
+//store gameId to identify which game is being played
+$gameId = $row["gameId"];
+// echo("gameRoomId:".$gameRoomId);
+// echo("gameid:".$gameId);
 
 
-//Insert player into Game Players Table
-$stmt3 = $pdo->prepare("INSERT INTO `gamePlayers`(`playerId`, `userName`, `points`, `gameRoomId`, `avatar`) VALUES  (NULL,'$userNamePlayer', '0', '$gameRoomId', '$avatar');");
+//Find How many players allowed in Game
+$stmt2 = $pdo->prepare("SELECT * FROM `games` WHERE `gameId` = '$gameId'");
+$stmt2->execute();
+$row2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+//Store the player Limit for the game in the game room
+$gamePlayerLimit = $row2["playerLimit"];
+//echo("gamePlayers:".$gamePlayerLimit);
 
+
+
+
+
+//Count how many players are already in the room. 
+$stmt3 = $pdo->prepare("SELECT COUNT(playerId) FROM `gamePlayers` WHERE `gameRoomId` = '$gameRoomId' ");
+//excecute statement to count
 $stmt3->execute();
+$count = $stmt3->fetch(PDO::FETCH_ASSOC);
+$activePlayers = $count["COUNT(playerId)"];
+//echo("activePlayers:".$activePlayers);
 
 
 
+//if Game room exists and theres room to be a player
+//COMPARE how many players allowed for game VERSUS how many gamePlayers are in room
+if (isset($gameRoomId) && ($activePlayers < $gamePlayerLimit)) {
 
-if (true){
+    //Insert player into Game Players Table
+    $stmt4 = $pdo->prepare("INSERT INTO `gamePlayers`(`playerId`, `userName`, `points`, `gameRoomId`, `avatar`) VALUES  (NULL,'$userNamePlayer', '0', '$gameRoomId', '$avatar');");
 
-    echo($gameRoomId);
+    $stmt4->execute();
+
     //store the players username in the session
     $_SESSION["userNamePlayer"] = $userNamePlayer;
 
+    echo($gameRoomId);
+    //echo("Welcome");
 
-} else {
+    //if game Room Exits && room is full
+} else if (isset($gameRoomId) && ($activePlayers >= $gamePlayerLimit)) {
+    echo ("ROOM FULL");
 
-    echo("The Game Room Does Not Exist");
+    //game Room Does not Exits
+} else if ($gameRoomId == NULL){
+
+    echo("Game Room Does Not Exist");
 
 }
 
